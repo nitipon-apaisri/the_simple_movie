ab375182b8df02526cf1626ad6a1b3374c0c66f5
nitipon <nitipon.apaisri@gmail.com>
2023-09-24 16:24:36 +0200

    Added a method to render the search results

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,45 @@
 "use client";
 import { useState } from "react";
-import { Button, Divider, Input, Space } from "antd";
+import { Button, Card, Col, Divider, Input, Row, Space } from "antd";
 import { Api } from "./services/api";
+import { movieType } from "./types/movieTypes";
+import Image from "next/image";
+const { Meta } = Card;
 const Home = () => {
     const api = new Api();
+    const imgUrl = "https://image.tmdb.org/t/p/original";
     const [searchQuery, setSearchQuery] = useState<string>("");
-    const [searchResults, setSearchResults] = useState<any[]>([]);
+    const [searchResults, setSearchResults] = useState<movieType[]>([]);
 
     const handleSearch = async () => {
         const results = await api.searchMovies(searchQuery);
-        setSearchResults(results);
+        setSearchResults(results.results);
     };
+
     return (
         <>
             <h1>Search a movie</h1>
             <Space>
-                <Input placeholder="Movie name" onChange={(e) => setSearchQuery(e.target.value)} />
+                <Input placeholder="Movie name" onChange={(e) => setSearchQuery(e.target.value)} onPressEnter={handleSearch} />
                 <Button type="primary" onClick={handleSearch}>
                     Search
                 </Button>
             </Space>
             <Divider />
             <h1>Search results</h1>
+            {searchResults.length > 0 ? (
+                <Row gutter={[16, 16]}>
+                    {searchResults.map((movie) => (
+                        <Col key={movie.id} span={4}>
+                            <Card hoverable cover={<Image alt={movie.title} src={imgUrl + movie.poster_path} width={360} height={360} style={{ objectFit: "cover" }} />}>
+                                <Meta title={movie.title} />
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            ) : (
+                "No results"
+            )}
         </>
     );
 };
